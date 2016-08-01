1423809bc6843821f39fc0bdc0c5957ee324981a
Gilles ZIELINSKI <gzielinski@mcnext.com>
2016-08-01 19:15:44 +0200

    Mistake

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ function errorCallback(msg, exceptionContent){
 //=========================================================
 // Bot Setup
 //=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+//server.listen(process.env.port || process.env.PORT || 3978, function () {
+server.listen(3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
   
 // Create chat bot
 var connector = new builder.ChatConnector({
